47a4d24a46a83f958c328c5173f64c89bb059fcb
jhulialeal <lealjhu@gmail.com>
2023-07-01 01:21:44 -0300

    adicionar livro na biblioteca

--- jhulialeal.js
+++ jhulialeal.js
@@ -13,9 +13,30 @@ Aluna: Jhulia de Souza Leal
 
 while (continua) {
     const pergunta = readlineSync.question(`O que deseja fazer?
-1 - Cadastrar livro
-2 - Listar livros
-3 - Remover livro
-4 - Sair\n
-Digite sua opção: `);
+    1 - Cadastrar livro
+    2 - Listar livros
+    3 - Remover livro
+    4 - Sair\n
+    Digite sua opção: `);
+    
+    switch (pergunta) {
+        case "1":
+            const titulo = readlineSync.question("Digite o título do livro: ");
+            
+            const autor = readlineSync.question("Digite o autor do livro: ");
+            
+            const data = readlineSync.question("Digite a data de publicação do livro: ");
+                
+            const livro = {
+                titulo,
+                autor,
+                data,
+            };
+            
+            biblioteca.push(livro);
+        break;
+        
+        default:
+            console.log("Opção inválida");
+    }
 }
